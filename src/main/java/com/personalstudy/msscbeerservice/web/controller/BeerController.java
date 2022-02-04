1941729008610a178e84a4fd48df504922221197
Vasilijs <v1ac95@gmail.com>
2022-02-04 22:41:23 +0200

    Added Validation and Error Handling. Closes #9

--- src/main/java/com/personalstudy/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/personalstudy/msscbeerservice/web/controller/BeerController.java
@@ -3,7 +3,14 @@ package com.personalstudy.msscbeerservice.web.controller;
 import com.personalstudy.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
@@ -19,14 +26,14 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto) {
 
         //todo impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeerById(@PathVariable UUID beerId, @RequestBody @Validated BeerDto beerDto) {
 
         //todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
